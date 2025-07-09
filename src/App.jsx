9bd72bf93a763459c6393964e586e8dccc86e3ea
Gowtham-K23 <gowthamk2394@gmail.com>
2025-07-09 13:55:42 +0530

    Changed effects in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,56 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from 'framer-motion';
+
 import Auth from "./pages/Auth";
 import Leftover from "./pages/Leftover";
 import Recipe from "./pages/Recipe";
 
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={
+          <motion.div
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -30 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Auth />
+          </motion.div>
+        } />
+        <Route path="/leftover" element={
+          <motion.div
+            initial={{ opacity: 0, scale: 0.95 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.95 }}
+            transition={{ duration: 0.4 }}
+          >
+            <Leftover />
+          </motion.div>
+        } />
+        <Route path="/recipe" element={
+          <motion.div
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -50 }}
+            transition={{ duration: 0.4 }}
+          >
+            <Recipe />
+          </motion.div>
+        } />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Auth />} />
-        <Route path="/leftover" element={<Leftover />} />
-        <Route path="/recipe" element={<Recipe />} />
-      </Routes>
+      <AnimatedRoutes />
     </Router>
   );
 }
